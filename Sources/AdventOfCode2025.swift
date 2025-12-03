f199916d755251ca963086fd6364c5ef71850008
Miles Bardon <milesbardon@gmail.com>
2025-12-03 18:42:35 +0000

    feat: Completing Day 3 (#4)
    * feat: Completing Part 1
    
    * feat: Completing Part 2

--- Sources/AdventOfCode2025.swift
+++ Sources/AdventOfCode2025.swift
@@ -7,12 +7,15 @@ struct AdventOfCode2025: AsyncParsableCommand {
 
     mutating func run() async throws {
         switch day {
-        case "day1":
-            let day1 = try await Day1(day: 1)
-            print("Part 1: \(day1.part1())\nPart 2: \(day1.part2())")
-        case "day2":
-            let day2 = try await Day2(day: 2)
-            print("Part 1: \(day2.part1())\nPart 2: \(day2.part2())")
+        case "1":
+            let day = try await Day1(day: 1)
+            print("Part 1: \(day.part1())\nPart 2: \(day.part2())")
+        case "2":
+            let day = try await Day2(day: 2)
+            print("Part 1: \(day.part1())\nPart 2: \(day.part2())")
+        case "3":
+            let day = try await Day3(day: 3)
+            print("Part 1: \(day.part1())\nPart 2: \(day.part2())")
         default:
             print("Day \(day) has not been solved yet")
         }
