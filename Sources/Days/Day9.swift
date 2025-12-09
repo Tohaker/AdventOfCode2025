f63798df34fd15ca8be0829697ab6512c4765764
Miles Bardon <milesbardon@gmail.com>
2025-12-09 14:33:06 +0000

    feat: [WIP] Starting Part 2

--- Sources/Days/Day9.swift
+++ Sources/Days/Day9.swift
@@ -1,18 +1,37 @@
 import Foundation
 
+struct Coord: Hashable & Equatable {
+    let x: Int
+    let y: Int
+
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.x)
+        hasher.combine(self.y)
+    }
+
+    static func == (lhs: Coord, rhs: Coord) -> Bool {
+        lhs.x == rhs.x && lhs.y == rhs.y
+    }
+}
+
 class Day9: BaseDay {
-    private func parseInput() -> [(x: Int, y: Int)] {
+    private func parseInput() -> [Coord] {
         self.input.map { line in
 
             let components = line.split(separator: ",")
             let x = Int(String(components[0]))!
             let y = Int(String(components[1]))!
 
-            return (x, y)
+            return Coord(x, y)
         }
     }
 
-    private func areaBetweenTwoPoints(_ a: (x: Int, y: Int), _ b: (x: Int, y: Int)) -> Int {
+    private func areaBetweenTwoPoints(_ a: Coord, _ b: Coord) -> Int {
         if a.x == b.x {
             return abs(a.y - b.y + 1)
         }
@@ -24,9 +43,74 @@ class Day9: BaseDay {
         return abs(a.x - b.x + 1) * abs(a.y - b.y + 1)
     }
 
+    private func tilesBetweenTwoPoints(_ a: Coord, _ b: Coord) -> [Coord] {
+        let minX = min(a.x, b.x)
+        let maxX = max(a.x, b.x)
+
+        let minY = min(a.y, b.y)
+        let maxY = max(a.y, b.y)
+
+        var tiles: [Coord] = []
+
+        for y in minY...maxY {
+            for x in minX...maxX {
+                tiles.append(Coord(x, y))
+            }
+        }
+
+        return tiles
+    }
+
+    private func floodFill(node: Coord, edges: [Coord], filled: inout [Coord]) {
+        if edges.contains(node) || filled.contains(node) {
+            return
+        }
+
+        filled.append(node)
+
+        floodFill(node: Coord(node.x, node.y + 1), edges: edges, filled: &filled)
+        floodFill(node: Coord(node.x, node.y - 1), edges: edges, filled: &filled)
+        floodFill(node: Coord(node.x + 1, node.y), edges: edges, filled: &filled)
+        floodFill(node: Coord(node.x - 1, node.y), edges: edges, filled: &filled)
+    }
+
+    private func mapGreenTiles(tileFloor: [Coord]) -> Set<Coord> {
+        // Find edges first, edges are connected in the order of the red tiles
+        var edges: [Coord] = []
+        var wrappedTileFloor = Array(tileFloor)
+        wrappedTileFloor.append(tileFloor[0])
+
+        for i in 0..<wrappedTileFloor.count - 1 {
+            edges.append(
+                contentsOf: tilesBetweenTwoPoints(wrappedTileFloor[i], wrappedTileFloor[i + 1]))
+        }
+
+        // Fill in between edges - mind the gap
+        var filled: [Coord] = []
+        // This will probably take us inside to the first corner
+        let firstNode = Coord(tileFloor[0].x + 1, tileFloor[0].y + 1)
+
+        floodFill(node: firstNode, edges: edges, filled: &filled)
+
+        return Set(filled).union(Set(edges))
+    }
+
     override func part1() -> Int {
         let tileFloor = parseInput()
         let tileCombinations = tileFloor.combinations(ofCount: 2).map { combo in
+            areaBetweenTwoPoints(combo[0], combo[1])
+        }
+
+        return tileCombinations.max(count: 1)[0]
+    }
+
+    override func part2() -> Int {
+        let redTiles = parseInput()
+        let greenTiles = mapGreenTiles(tileFloor: redTiles)
+
+        let tileCombinations = redTiles.combinations(ofCount: 2).filter({ combo in
+            tilesBetweenTwoPoints(combo[0], combo[1]).allSatisfy(greenTiles.contains)
+        }).map { combo in
             areaBetweenTwoPoints(combo[0], combo[1])
         }
 
--- Tests/AdventOfCode2025Tests/Days/Day9Test.swift
+++ Tests/AdventOfCode2025Tests/Days/Day9Test.swift
@@ -22,3 +22,24 @@ import Testing
 
     #expect(day.part1() == 4_763_932_976)
 }
+
+@Test func day9part2ex1() {
+    let input = """
+        7,1
+        11,1
+        11,7
+        9,7
+        9,5
+        2,5
+        2,3
+        7,3
+        """.components(separatedBy: "\n")
+
+    #expect(Day9(input: input).part2() == 24)
+}
+
+@Test func day9part2() async throws {
+    let day = try await Day9(day: 9)
+
+    #expect(day.part2() == 0)
+}
