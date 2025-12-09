7a2dc0713ee8820dd46f809eb9f6a646a39a083e
Miles Bardon <milesbardon@gmail.com>
2025-12-09 00:38:33 +0000

    feat: Completing Part 2

--- Sources/Days/Day8.swift
+++ Sources/Days/Day8.swift
@@ -56,13 +56,14 @@ class Day8: BaseDay {
             let distance = lhs.distance(to: rhs)
 
             return (lhs, rhs, distance)
-        }.min(count: self.connections, sortedBy: { lhs, rhs in lhs.distance < rhs.distance })
+        }
     }
 
     override func part1() -> Int {
         let junctionBoxes = parseJunctionBoxes()
 
-        let pairs = createJunctionBoxPairs(junctionBoxes)
+        let pairs = createJunctionBoxPairs(junctionBoxes).min(
+            count: self.connections, sortedBy: { a, b in a.distance < b.distance })
 
         var circuits: [Set<JunctionBox>] = []
 
@@ -94,6 +95,47 @@ class Day8: BaseDay {
         }
 
         return circuits.sorted(by: { $0.count > $1.count })[0..<3].reduce(1, { $0 * $1.count })
+    }
 
+    override func part2() -> Int {
+        let junctionBoxes = parseJunctionBoxes()
+
+        let pairs = createJunctionBoxPairs(junctionBoxes).sorted { a, b in a.distance < b.distance
+        }
+
+        var circuits: [Set<JunctionBox>] = []
+
+        for closestPair in pairs {
+            // Find all circuits with at least one of the pair
+            let circuitsContainingBoxes = circuits.filter {
+                $0.contains(closestPair.a) || $0.contains(closestPair.b)
+            }
+
+            // if more than one exists, join them together
+            if circuitsContainingBoxes.count > 1 {
+                let newCircuit = Set<JunctionBox>(
+                    circuitsContainingBoxes.flatMap {
+                        $0
+                    })
+
+                circuits.removeAll { circuitsContainingBoxes.contains($0) }
+                circuits.append(newCircuit)
+
+            } else {
+                if let circuit = circuits.firstIndex(where: { $0.contains(closestPair.a) }) {
+                    circuits[circuit].insert(closestPair.b)
+                } else if let circuit = circuits.firstIndex(where: { $0.contains(closestPair.b) }) {
+                    circuits[circuit].insert(closestPair.a)
+                } else {
+                    circuits.append(Set([closestPair.a, closestPair.b]))
+                }
+            }
+
+            if circuits.filter({ $0.count == junctionBoxes.count }).count == 1 {
+                return closestPair.a.x * closestPair.b.x
+            }
+        }
+
+        return 0
     }
 }
--- Tests/AdventOfCode2025Tests/Days/Day8Test.swift
+++ Tests/AdventOfCode2025Tests/Days/Day8Test.swift
@@ -37,3 +37,38 @@ import Testing
 
     #expect(day.part1() == 68112)
 }
+
+@Test func day8part2ex1() {
+    let input = """
+        162,817,812
+        57,618,57
+        906,360,560
+        592,479,940
+        352,342,300
+        466,668,158
+        542,29,236
+        431,825,988
+        739,650,466
+        52,470,668
+        216,146,977
+        819,987,18
+        117,168,530
+        805,96,715
+        346,949,466
+        970,615,88
+        941,993,340
+        862,61,35
+        984,92,344
+        425,690,689
+        """.components(separatedBy: "\n")
+
+    let day = Day8(input: input)
+
+    #expect(day.part2() == 25272)
+}
+
+@Test func day8part2() async throws {
+    let day = try await Day8(day: 8)
+
+    #expect(day.part2() == 44_543_856)
+}
